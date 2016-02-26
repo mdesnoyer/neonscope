707d1b63a66058833ee8dee5f0848f7efde7527c
Ed Henderson <ed@teamhendo.com>
2016-02-26 11:36:11 -0800

    Neonscope: Add Include/Exclude functionality #578 - https://neonlabs.atlassian.net/browse/NEON-578 - added extra check to look for `thumb.urls` before fetching `thumb.urls[0]` and also checking `thumb` - incorporating some API changes (`model_score` now `neon_score`) - Added NeonScores support, `formatModelScore` is now `formatNeonScore` and numbers added for 0..99 - added thumbnail id to the thumbnail to use in the API call to enable/disable the thumbnail - added a thumbnail state (enabled, disabled, thinking) and added `toggleThumbnail` function - whitespace tidy - removed use of data attributes (unreliable), straight to `attr` - Changes   - renamed `BASE_URL` -> `BASE_NEON_IMAGES_URL`   - `enabled` -> `thumbnail-status`   - `EXTENSION_STATES` -> `EXTENSION_STATE`   - `assembled_url` -> `assembledUrl`   - `serving-frac` -> `serving-fraction`

--- js/shared.js
+++ js/shared.js
@@ -1,16 +1,23 @@
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 var APP_NAME = 'neonscope',
+	NEONSCORE_NAME = 'NeonScore',
+	UNKNOWN_STRING = '?',
     PROCESS_PARTICLES_INTERVAL_MS = 10000, // 10 seconds
     REFRESH_TOKEN_INTERVAL_MS = 1000 * 60 * 10, // 10 minutes
     SCRIPTS = {
         INJECT: 'js/inject.js',
         JQUERY: 'js/lib/jquery-2.2.0.min.js'
     },
-    EXTENSION_STATES = {
-    DEAD: 'dead',
-    PULSE: 'pulse',
-    ALIVE: 'alive'
+    EXTENSION_STATE = {
+	    DEAD: 'dead',
+	    PULSE: 'pulse',
+	    ALIVE: 'alive'
+    }
+    THUMBNAIL_STATE = {
+	    ENABLED: 'enabled', // 1
+	    THINKING: 'thinking',
+	    DISABLED: 'disabled' // 0
     }
     RIPENESS_STATE = {
         RAW: 'W',
@@ -56,13 +63,9 @@ var APP_NAME = 'neonscope',
         WINNING_BACKGROUND: '*[style*="{{ winning_selector }}"]',
         WINNING_MASK: CLASSES.BASE + 'winning-mask'
     },
+	NEONSCORES = [0.0, 0.155, 0.194, 0.222, 0.247, 0.269, 0.289, 0.31, 0.328, 0.347, 0.363, 0.381, 0.396, 0.41, 0.424, 0.438, 0.452, 0.465, 0.479, 0.492, 0.504, 0.517, 0.531, 0.543, 0.555, 0.567, 0.579, 0.59, 0.602, 0.613, 0.624, 0.635, 0.647, 0.658, 0.67, 0.681, 0.693, 0.704, 0.715, 0.727, 0.739, 0.751, 0.763, 0.776, 0.79, 0.801, 0.813, 0.826, 0.837, 0.85, 0.863, 0.876, 0.889, 0.901, 0.915, 0.928, 0.943, 0.957, 0.972, 0.986, 1.002, 1.017, 1.032, 1.047, 1.064, 1.08, 1.096, 1.115, 1.132, 1.151, 1.17, 1.191, 1.212, 1.233, 1.255, 1.277, 1.301, 1.324, 1.35, 1.376, 1.402, 1.432, 1.461, 1.494, 1.529, 1.566, 1.604, 1.646, 1.69, 1.741, 1.792, 1.847, 1.917, 1.991, 2.08, 2.187, 2.315, 2.474, 2.703, 3.131],
     beacon = function(msg) {
         console.log('[' + APP_NAME + ']', msg);
-    },
-    getRandomIntInclusive = function(min, max) {
-        // Returns a random integer between min (included) and max (included)
-        // Using Math.round() will give you a non-uniform distribution!
-        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 ;
 
